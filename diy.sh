da6a931f785b57a6458e43c247195252c6d68cc9
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-07-06 21:50:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -14,6 +14,7 @@ svn_export() {
 #cp -f ./patch/02_network target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 cp -f $GITHUB_WORKSPACE/patch/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4/102-mt7621-fix-cpu-clk-add-clkdev.patch
 cp -f $GITHUB_WORKSPACE/patch/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+cp -f $GITHUB_WORKSPACE/patch/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.15/322-mt7621-fix-cpu-clk-add-clkdev.patch
 
 # 删除冲突软件和依赖
 #rm -rf feeds/packages/lang/golang 
